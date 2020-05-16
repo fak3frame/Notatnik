6166ea86604a72b2402dd817db4dfe1d2cb25287
Kamil Z <kamil7745@gmail.com>
2020-05-16 13:26:09 +0200

    implementacja bazy

--- app/src/main/java/pl/wsei/notatnik/DataBaseHelper.kt
+++ app/src/main/java/pl/wsei/notatnik/DataBaseHelper.kt
@@ -1,5 +1,8 @@
 package pl.wsei.notatnik
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
 
 
@@ -19,4 +22,18 @@ object BasicCommand{
                 "${TableInfo.TABLE_COLUMN_TITLE} TEXT NOT NULL," +
                 "${TableInfo.TABLE_COLUMN_MESSAGE} TEXT NOT NULL)"
     const val SQL_DELETE_TABLE = "DROP TABLE IF EXIST ${TableInfo.TABLE_NAME}"
+}
+
+class DataBaseHelper(context: Context): SQLiteOpenHelper(context, TableInfo.TABLE_NAME, null, 1){
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(BasicCommand.SQL_CREATE_TABLE)
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL(BasicCommand.SQL_DELETE_TABLE)
+        onCreate(db)
+    }
+
+
 }
